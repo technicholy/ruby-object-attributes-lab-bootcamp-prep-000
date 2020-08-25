1762485cacc636e00833e3aec6b16bdf80a814bd
Clayton Pierce <bamer78@gmail.com>
2020-08-25 03:02:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,10 +3,16 @@ class Person
     @name = name
     @job = job
   end
-  def name=(name)
+  def name
     @name
   end
-  def job=(job)
+  def name=(newname)
+    @name = newname
+  end
+  def job
     @job
+  end
+  def job=(newjob)
+    @job = newjob
   end
 end
